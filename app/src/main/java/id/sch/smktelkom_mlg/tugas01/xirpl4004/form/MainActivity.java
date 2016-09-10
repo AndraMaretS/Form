f81800301de16c887a0bc2f336080725e8c748b3
Andra Maret <andramaret4@gmail.com>
2016-09-10 15:29:46 +0700

    .menampilkan editText

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4004/form/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4004/form/MainActivity.java
@@ -2,12 +2,44 @@ package id.sch.smktelkom_mlg.tugas01.xirpl4004.form;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etNis, etNama, etTgl;
+    Button buttonOK;
+    TextView tvHasil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNis = (EditText) findViewById(R.id.editTextNis);
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etTgl = (EditText) findViewById(R.id.editTextTglLhr);
+        buttonOK = (Button) findViewById(R.id.buttonOk);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doEditText();
+            }
+        });
     }
+
+    private void doEditText() {
+        String nis = etNis.getText().toString();
+        String nama = etNama.getText().toString();
+        String tgl = etTgl.getText().toString();
+        tvHasil.setText("NIS                    : " + nis + "\n" +
+                "Nama                  : " + nama + "\n" +
+                "Tanggal Lahi   : " + tgl);
+    }
+
+
 }
